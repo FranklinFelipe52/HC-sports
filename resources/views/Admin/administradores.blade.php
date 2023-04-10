374787729b2a8818f9d1b6865b32a1df8e11cf6a
Felipe <f.franklin@escolar.ifrn.edu.br>
2023-04-10 13:12:56 -0300

    atualização 2

--- resources/views/Admin/administradores.blade.php
+++ resources/views/Admin/administradores.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -21,9 +22,9 @@
   <!-- grid principal -->
   <div class="grid grid-cols-1 sm:grid-cols-main-colapsed lg:grid-cols-main-expanded grid-rows-main-mobile sm:grid-rows-1 h-screen w-full">
 
-
-    <!-- Menu lateral -->
-    <div class="border-t sm:border-t-0 order-2 sm:order-1 relative border-r border-gray-5">
+    
+<!-- Menu lateral -->
+<div class="border-t sm:border-t-0 order-2 sm:order-1 relative border-r border-gray-5">
       @include('components.admin.menu_lateral');
     </div>
 
@@ -53,24 +54,24 @@
 
                 <input type="text" placeholder="Pesquise por um atleta usando cpf ou nome" name="s" class="text-sm text-gray-1 placeholder:text-gray-3 p-2 rounded-lg pl-12 w-full border border-gray-5 focus:border-brand-a1 focus:outline-1 focus:outline-offset-0 focus:outline-brand-a1 transition">
                 <button type="submit" class="absolute top-[14%] left-3 bg-white">
-                  <img src="/images/svg/search.svg" alt="">
+                  <img src="/frontend/dist/images/svg/search.svg" alt="">
                 </button>
 
               </form>
               @if (Session('admin')->rule->id == 1)
-                <form id="filter_uf" class="relative">
+              <form id="filter_uf" class="relative">
 
-                  <select onchange="document.getElementById('filter_uf').submit()" class="w-full min-w-[195px] px-4 py-2 rounded-lg bg-white border border-gray-5 focus:border-brand-a1 focus:outline-brand-a1 text-gray-1 text-sm placeholder:text-gray-3 appearance-none" name="uf" id="filtro_atletas_page">
-                    <option value disabled selected>UF</option>
-                    @foreach ($federative_units as $federative_unit)
-                      <option {{ Request::get('uf') && Request::get('uf') == $federative_unit->id ? 'selected' : '' }} value="{{ $federative_unit->id }}">{{ $federative_unit->initials }}</option>
-                    @endforeach
-                  </select>
-                  <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-                    <img src="/images/svg/chevron-down.svg" alt="" />
-                  </div>
+                <select onchange="document.getElementById('filter_uf').submit()" class="w-full min-w-[195px] px-4 py-2 rounded-lg bg-white border border-gray-5 focus:border-brand-a1 focus:outline-brand-a1 text-gray-1 text-sm placeholder:text-gray-3 appearance-none" name="uf" id="filtro_atletas_page">
+                  <option value disabled selected>UF</option>
+                  @foreach ($federative_units as $federative_unit )
+                  <option {{ ( Request::get('uf') && (Request::get('uf') == $federative_unit->id) ) ? 'selected' : '' }} value="{{$federative_unit->id}}">{{$federative_unit->initials}}</option>
+                  @endforeach
+                </select>
+                <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                  <img src="/frontend/dist/images/svg/chevron-down.svg" alt="" />
+                </div>
 
-                </form>
+              </form>
               @endif
 
             </div>
@@ -106,8 +107,14 @@
 
             <!-- Table body -->
             <div class="min-w-[600px] h-fit overflow-auto border border-t-0 border-gray-5 rounded-b-lg">
-              @foreach ($administradores as $administrador)
-                <!-- Table row -->
+            @foreach ($administradores as $administrador )
+              <!-- Table row -->
+              <div role="row" class="px-4 grid grid-cols-12 border-b border-b-gray-5 last:border-b-0">
+                <div role="cell" class="py-3 flex items-center col-span-3">
+                  <p class="text-sm font-semibold text-gray-2">
+                    <?php echo preg_replace('/^([[:digit:]]{3})([[:digit:]]{3})([[:digit:]]{3})([[:digit:]]{2})$/', '$1.$2.$3-$4', $administrador->cpf); ?>
+                  </p>
+                </div>
                 <div role="cell" class="pr-2 py-3 flex items-center col-span-4">
                   <p class="text-sm font-semibold text-gray-2">
                   {{$administrador->nome_completo}}
@@ -139,18 +146,18 @@
 
         <!-- Paginação da tabela -->
         <div class="flex justify-between pt-6 pb-4 sm:pb-16">
-          {{ $administradores->appends([
-                  's' => request()->get('s', ''),
-                  'uf' => request()->get('uf', ''),
-              ])->links() }}
+        {{$administradores->appends([
+                    's' => request()->get('s', ''),
+                    'uf' => request()->get('uf', '')
+                    ])->links()}}
           <div>
             <p class="text-gray-3 text-sm font-normal">
               @if (Count($administradores) > 1)
-                {{ Count($administradores) }} Administradores exibidos
+              {{Count($administradores)}} Administradores exibidos
               @else
-                {{ Count($administradores) }} Administrador exibido
+              {{Count($administradores)}} Administrador exibido
               @endif
-
+           
             </p>
           </div>
         </div>
