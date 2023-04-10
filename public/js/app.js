b5bd1d3bcf5c91ca1ab8f084d052cee53295d3a1
Zadoque Teófilo <zadoque.dev@gmail.com>
2023-04-10 18:30:15 -0300

    feat: Adicionando módulo da mascara de cpf

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 import initHidePassword from "./modules/hidePassword.js";
 import initModal from "./modules/modal.js";
+import initCpfMask from "./modules/cpfMask.js";
 
 initHidePassword();
 initModal();
+initCpfMask();
