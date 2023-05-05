627b0eabddcd0e1d3922f42a40c657bd33ffe10e
Zadoque Teófilo <zadoque.dev@gmail.com>
2023-05-05 18:26:41 -0300

    adicionando endereço no retorno da rota da api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,18 +8,20 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function getUserByCPF($cpf){
-        try{
+    public function getUserByCPF($cpf)
+    {
+        try {
             $user = User::where('cpf', $cpf)
-            ->select('nome_completo', 'data_nasc', 'cpf', 'email', 'is_pcd', 'sexo',)->get();
+                ->select()
+                ->join('addresses', 'addresses.user_id', 'users.id')
+                ->join('federative_units', 'federative_units.id', 'addresses.federative_unit_id')->get();
             error_log(Count($user));
-            if(Count($user) == 0){
-                return response('',404);
-            } 
+            if (Count($user) == 0) {
+                return response('', 404);
+            }
             return $user;
-
-        } catch(Exception $e){
-            return response('',500);
+        } catch (Exception $e) {
+            return response('', 500);
         }
     }
 }
