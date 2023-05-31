423689f4331ff7283beaa9e2dc978e015196a65f
Zadoque Teófilo <zadoque.dev@gmail.com>
2023-05-31 14:31:01 -0300

    feat: exibindo página de visualização de inscrição

--- resources/views/Admin/registration.blade.php
+++ resources/views/Admin/registration.blade.php
@@ -1,25 +1,42 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+@extends('Admin.base')
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Atleta - Comprovante de inscrição</title>
+@section('title', 'Atleta - Comprovante de inscrição ')
 
-  <!-- fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+@section('content')
 
-  <!-- css -->
-  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-  <link rel="stylesheet" href="/frontend/dist/css/style.css">
-</head>
+{{-- modal validar inscrição --}}
+    <div id="modal-validar-inscricao-{{ $registration->id }}" class="hidden">
+      <div class="flex h-screen w-full fixed bottom-0 bg-black bg-opacity-60 z-50 justify-center items-center">
+        <div class="bg-white mx-3 p-3 md:p-6 rounded-lg w-full max-w-[500px]">
+          <!-- modal header -->
+          <div class="text-gray-1 text-lg md:text-xl font-semibold">
+            <p>
+              Confirmar validação de Inscrição
+            </p>
+          </div>
+          <hr class="my-4">
 
-<body class="h-screen">
+          <!-- modal body -->
+          <div class="text-gray-1 text-base">
+            <p>
+              Confirme se realmente deseja validar essa inscrição
+            </p>
+          </div>
 
-  <!-- modal -->
+          <!-- modal footer - actions -->
+          <div class="flex justify-end gap-4 flex-wrap mt-10">
+            <button data-modalId="modal-validar-inscricao-{{ $registration->id }}" data-action="close" class="bg-white border border-black text-v1 text-sm font-poppins font-bold w-full sm:w-fit py-2.5 px-4 rounded flex justify-center items-center gap-2.5 hover:ring-2 hover:ring-gray-4 hover:ring-opacity-50 transition disabled:opacity-50 disabled:hover:ring-0">
+              Cancelar
+            </button>
+            <a href="/admin/pagamentos/confirm/{{ $registration->payment->id }}" class="bg-brand-a1 border border-brand-a1 text-white text-sm font-poppins font-bold w-full sm:w-fit py-2.5 px-4 rounded flex justify-center items-center gap-2.5 hover:ring-2 hover:ring-v1 hover:ring-opacity-50 transition disabled:opacity-50 disabled:hover:ring-0">
+              Validar
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+ {{-- modal excluir inscrição --}}
   <div id="modal" class="hidden">
     <div class="flex h-screen w-full fixed bottom-0 bg-black bg-opacity-60 z-50 justify-center items-center">
       <div class="bg-white mx-3 p-3 md:p-6 rounded-lg w-full max-w-[532px]">
@@ -75,18 +92,32 @@
         <header class="pt-8 pb-6 space-y-6">
           <nav aria-label="Breadcrumb" class="flex items-center flex-wrap gap-2">
             <div>
-              <a href="/src/pages/admin/atletas.html" class="text-xs text-gray-1 block hover:underline">
+              <a href="/admin/users" class="text-xs text-gray-1 block hover:underline">
                 Atletas
               </a>
             </div>
             <img src="/images/svg/chevron-left-breadcrumb.svg" alt="">
+            <div>
+              <a href="/admin/users/{{ $registration->user->id }}" class="text-xs text-gray-1 block hover:underline">
+                @if ($registration->user->nome_completo)
+                  {{ $registration->user->nome_completo }}
+                @else
+                  {{ $registration->user->email }}
+                @endif
+              </a>
+            </div>
+            <img src="/images/svg/chevron-left-breadcrumb.svg" alt="">
             <div aria-current="page" class="text-xs text-brand-a1 font-semibold">
-              Comprovante de inscrição
+              Visualização da inscrição
             </div>
           </nav>
           <div class="flex gap-4 items-center flex-wrap">
             <h1 class="text-lg text-gray-1 font-poppins font-semibold">
-              {{ $registration->user->nome_completo }}
+              @if ($registration->user->nome_completo)
+                {{ $registration->user->nome_completo }}
+              @else
+                {{ $registration->user->email }}
+              @endif
             </h1>
             <div class="@if ($registration->status_regitration->id == 1) bg-feedback-fill-green @elseif ($registration->status_regitration->id == 3) bg-feedback-fill-purple @endif     py-1 px-1.5 rounded-full inline-block w-fit h-fit">
               <p class="@if ($registration->status_regitration->id == 1) text-feedback-green-1 @elseif ($registration->status_regitration->id == 3) text-feedback-purple @endif text-xs">
@@ -160,33 +191,80 @@
                 </p>
               </div>
             </div>
-            <!--<div class="grid grid-cols-2 gap-2 sm:gap-1 p-4 sm:px-6 border-b border-gray-5 last:border-b-0">
+            <div class="grid grid-cols-2 gap-1 p-4 sm:px-6 border-b border-gray-5 last:border-b-0">
               <div class="col-span-2 sm:col-span-1">
                 <p class="text-sm text-gray-1 font-semibold">
-                  Comprovante de pagamento
+                  Status
                 </p>
               </div>
               <div class="col-span-2 sm:col-span-1">
-                <button class="inline-flex items-center gap-3 bg-fill-base p-2 pr-8 rounded-lg hover:bg-gray-6 transition">
-                  <div class="w-[30px] h-[30px]">
-                    <img src="/images/svg/link.svg" class="h-full w-full object-cover" alt="">
-                  </div>
-                  <p class="text-sm text-gray-1">
-                    comprovante-inscrição.pdf
-                  </p>
-                </button>
+                <p class="text-sm text-gray-2 font-normal">
+                  {{ $registration->status_regitration->status }}
+                </p>
               </div>
-            </div>-->
+            </div>
+            <div class="grid grid-cols-2 gap-1 p-4 sm:px-6 border-b border-gray-5 last:border-b-0">
+              <div class="col-span-2 sm:col-span-1">
+                <p class="text-sm text-gray-1 font-semibold">
+                  Valor
+                </p>
+              </div>
+              <div class="col-span-2 sm:col-span-1">
+                <p class="text-sm text-gray-2 font-normal">
+                  @if (!is_null($registration->Payment->mount))
+                    <?php echo "R$ " . number_format($registration->Payment->mount, 2, ',', ''); ?>
+                  @else
+                    -
+                  @endif
+                </p>
+              </div>
+            </div>
+            <!--<div class="grid grid-cols-2 gap-2 sm:gap-1 p-4 sm:px-6 border-b border-gray-5 last:border-b-0">
+                <div class="col-span-2 sm:col-span-1">
+                  <p class="text-sm text-gray-1 font-semibold">
+                    Comprovante de pagamento
+                  </p>
+                </div>
+                <div class="col-span-2 sm:col-span-1">
+                  <button class="inline-flex items-center gap-3 bg-fill-base p-2 pr-8 rounded-lg hover:bg-gray-6 transition">
+                    <div class="w-[30px] h-[30px]">
+                      <img src="/images/svg/link.svg" class="h-full w-full object-cover" alt="">
+                    </div>
+                    <p class="text-sm text-gray-1">
+                      comprovante-inscrição.pdf
+                    </p>
+                  </button>
+                </div>
+              </div>-->
 
 
           </div>
           <div class="flex flex-wrap justify-between gap-6">
-            <button data-modalId="modal" data-action="open" class="flex items-center justify-center sm:justify-start gap-4 w-full sm:w-fit px-4 py-2.5 rounded border-[1.5px] border-brand-v1 hover:ring-2 hover:ring-brand-v1 hover:ring-opacity-50 bg-white transition">
+            {{-- <button data-modalId="modal" data-action="open" class="flex items-center justify-center sm:justify-start gap-4 w-full sm:w-fit px-4 py-2.5 rounded border-[1.5px] border-brand-v1 hover:ring-2 hover:ring-brand-v1 hover:ring-opacity-50 bg-white transition">
               <img src="/images/svg/trash.svg" alt="">
               <p class="text-brand-v1 text-sm font-bold font-poppins">
                 Excluir Inscrição
               </p>
-            </button>
+            </button> --}}
+            <div></div>
+            <div class="flex gap-4">
+              @if (!($registration->type_payment_id == 2 && $registration->status_regitration_id == 1))
+                @if ($registration->modalities_id == 9 || $registration->modalities_id == 10)
+                  <a href="/admin/registration/update/{{ $registration->id }}?gender={{ $registration->user->sexo }}" class="flex items-center justify-center w-fit text-sm font-bold text-brand-a1 p-2 px-4 rounded-md border border-brand-a1 hover:ring-2 hover:ring-brand-a1 hover:ring-opacity-50 disabled:hover:ring-0 disabled:opacity-50 disabled:cursor-not-allowed transition">
+                    Editar
+                  </a>
+                @else
+                  <a href="/admin/registration/update/{{ $registration->id }}" class="flex items-center justify-center w-fit text-sm font-bold text-brand-a1 p-2 px-4 rounded-md border border-brand-a1 hover:ring-2 hover:ring-brand-a1 hover:ring-opacity-50 disabled:hover:ring-0 disabled:opacity-50 disabled:cursor-not-allowed transition">
+                    Editar
+                  </a>
+                @endif
+              @endif
+              @if (!($registration->status_regitration->id == 1) && Session('admin')->rule->id == 1)
+                <button data-modalId="modal-validar-inscricao-{{ $registration->id }}" data-action="open" class="text-center text-xs font-semibold text-white p-2 rounded bg-brand-a1 border border-brand-a1 hover:ring-2 hover:ring-brand-a1 hover:ring-opacity-50 transition">
+                  Validar inscrição
+                </button>
+              @endif
+            </div>
           </div>
         </div>
       </div>
@@ -197,7 +275,6 @@
 <script type="module" src="/frontend/dist/js/index.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 <script>
-
     if ('{{ session('erro') }}') {
         showErrorToastfy('{{ session('erro') }}');
     }
@@ -238,6 +315,5 @@
         }).showToast();
     }
 </script>
-</body>
 
-</html>
+@endsection
