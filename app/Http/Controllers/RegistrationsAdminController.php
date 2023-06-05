f6fa6f33d1fcec49a219f6836c679f53b30fdb9e
Franklin Felipe <f.franklin@escolar.ifrn.edu.br>
2023-06-05 13:45:31 -0300

    adicionando tenis com 2 inscrições

--- app/Http/Controllers/RegistrationsAdminController.php
+++ app/Http/Controllers/RegistrationsAdminController.php
@@ -124,7 +124,7 @@ class RegistrationsAdminController extends Controller
                         return back()->withInput();
                     }
                     foreach ($user->registrations as $registration) {
-                        if(!($modalidade->id == 16) && $registration->modalities->id == $modalidade->id){
+                        if(!(($modalidade->id == 16) || ($modalidade->id == 13) ) && $registration->modalities->id == $modalidade->id){
                             session()->flash('erro', 'Usuário já tem inscrição nessa modalidade');
                             return back()->withInput();
                         }
