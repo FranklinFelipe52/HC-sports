e35f792f19e4b01e716c9fb23fc0a48710935bb2
Franklin Felipe <f.franklin@escolar.ifrn.edu.br>
2023-06-16 13:41:52 -0300

    edit ajustes

--- resources/views/components/edit_notification.blade.php
+++ resources/views/components/edit_notification.blade.php
@@ -5,7 +5,10 @@
   background-color: rgb(255 46 46 / var(--tw-bg-opacity));
 }
 </style>
-<div class="bg-red-3 w-full py-2 px-6 flex justify-end">
+<div class="bg-red-3 w-full py-2 px-6 flex justify-between">
+    <p class="text-white text-xs font-poppins">
+        Seus dados estão incompletos, complete os dados para ser validado no evento
+    </p>
     <button onclick="window.open('/profile/update', '_self')"  class="text-white border border-white px-2 py-0.5 text-xs font-poppins hover:ring-1 hover:ring-white hover:ring-opacity-50 transition">
       Editar perfil
     </button>
