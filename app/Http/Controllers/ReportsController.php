782b9032dc19e7335f8e11a89c47dc979f41ddca
Franklin Felipe <f.franklin@escolar.ifrn.edu.br>
2023-06-23 16:04:00 -0300

    script e-mail corrida

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ColumnsCreateReports;
+use App\Mail\ConfirmRegistrationRace;
 use App\Mail\ConfirmUser;
 use App\Models\registration;
 use App\Models\status_regitration;
@@ -125,7 +126,7 @@ class ReportsController extends Controller
         }
     }
 
-    public function scriptUsers(){
+    public function scriptUserss(){
         try{
             $users = User::where('registered', 0)->get();
             foreach ($users as $user) {
@@ -137,6 +138,126 @@ class ReportsController extends Controller
                 Mail::to($user->email)->send(new ConfirmUser($user, $senha));
             }
             return redirect('/admin/dashboard');
+        } catch (Exception $e){
+            dd($e);
+        }
+    }
+    public function scriptUsers(){
+        try{
+            $emails = ['aline.040197@gmail.com',
+            'marinaribeirodealmeida@hotmail.com',
+            'adervaladv@hotmail.com',
+            'henryque.resende@gmail.com',
+            'fabiofariasantos.adv@gmail.com',
+            'gustavodeassissouza@gmail.com',
+            'ludmillla@hotmail.com',
+            'joicekellysoares@gmail.com',
+            'advogado.heliomorato@gmail.com',
+            'doutorajuliane@gmail.com',
+            'paulohenrique@pinheiroadvogados.net',
+            'luhelenafranca@hotmail.com',
+            'vilmarprocopio@outlook.com',
+            'caionaves2@gmail.com',
+            'francoalmeidaadv@gmail.com',
+            'eduardololli.adv@gmail.com',
+            'silvestre132@gmail.com',
+            'jgadv@live.com',
+            'gabriellabandeira12229@gmail.com',
+            'amaralbritto@gmail.com',
+            'claudio.dias@peixotoediasadvogados.com',
+            'claudineia.pereira@jacocoelho.com.br',
+            'tomaz963@hotmail.com',
+            'ernane.nardelli@gmail.com',
+            'romulo.almeida@jacocoelho.com.br',
+            'mariacarolina.balestra@jacocoelho.com.br',
+            'tarcilla.faria@jacocoelho.com.br',
+            'ludmilla.coelho@jacocoelho.com.br',
+            'karynnemayer@gmail.com',
+            'matheus.coelho@jacocoelho.com.br',
+            'joicekellysoares@gmail.com',
+            'lucimer.coelho@jacocoelho.com.br',
+            'ferdinand_felipe@hotmail.com',
+            'danilorodriguesdesouza7@gmail.com',
+            'noiltodr@hotmail.com',
+            'csizervincio@gmail.com',
+            'gesnersouto@gmail.com',
+            'rafaelamorimsa@gmail.com',
+            'thalitamonferrari@gmail.com',
+            'domerviljunior@gmail.com',
+            'myllenasouzalins@gmail.com',
+            'talitahayasaki@gmail.com',
+            'paula.andreia@ahbnadvogados.com.br',
+            'claudiodelano@gmail.com',
+            'isabellafb3braga@gmail.com',
+            'ivanalmascarenhas@gmail.com',
+            'filipe.denki@laramartinsadvogados.com.br',
+            'joaogabrielsts@gmail.com',
+            'davigualbertoadv@hotmail.com',
+            'fabio.aso@gmail.com',
+            'thiagohenriqueresende@gmail.com',
+            'laraavila95@gmail.com',
+            'breno@silvadiasadvogados.com.br',
+            'jessica-cms@live.com',
+            'valeriaanzai.adv@gmail.com',
+            'heitorhourani03@gmail.com',
+            'carlafernanda245@gmail.com',
+            'adv.kahikonofre@hotmail.com',
+            'neto.ademario@gmail.com',
+            'biancadap@hotmail.com',
+            'fabiofariasantos.adv@gmail.com',
+            'luciana@recordtvgoias.com.br',
+            'henrique@lola.beer',
+            'alexandre@bandnewsgoiania.com.br',
+            'elianaqueirozz@hotmail.com',
+            'alexaugusto.adv@gmail.com',
+            'elisamarodrigues.adv@gmail.com',
+            'gabrieldiasmota99@gmail.com',
+            'cesarcg@ymail.com',
+            'titiorules@gmail.com',
+            'adangone@hotmail.com',
+            'gustavofat1996@gmail.com',
+            'jsp.paiva@gmail.com',
+            'gustavocastroadvogado@gmail.com',
+            'deborateodoroalmeida@gmail.com',
+            'dramonicaleite@hotmail.com',
+            'nathaliacaetano1413@gmail.com',
+            'joaopedrojube@hotmail.com',
+            'euripedes.emiliano@hotmail.com',
+            'marianabd@hotmail.com',
+            'natygobbo@hotmail.com',
+            'ativa.fernando@hotmail.com',
+            'anacfbrandao24@gmail.com',
+            'cyncardoso23@gmail.com',
+            'nathalia.fds1920@gmail.com',
+            'adv.pedrobispo@gmail.com',
+            'pedropaulovicentee@gmail.com',
+            'felipe.belchior12@gmail.com',
+            'gabreelcl@hotmail.com',
+            'cesar_breno@msn.com',
+            'lucimarlelesadv@gmail.com',
+            'mikhaelmoraes@gmail.com',
+            'lulu_portella@hotmail.com',
+            'leticiabeatrizmg@hotmail.com',
+            'adrianocap3i13@yahoo.com.br',
+            'adv.juniorribeiro@gmail.com',
+            'ludmilla.coelho@jacocoelho.com.br',
+            'larissagodoysinha.adv@gmail.com',
+            'juhliocsl@gmail.com',
+            'paulormdecaris28@gmail.com',
+            'peaga0803@gmail.com',
+            'filipebarbosa.jornalista@gmail.com',
+            'wpasquetto@gmail.com',
+            'matheus.coelho@jacocoelho.com.br',
+            'ladetos@gmail.com',
+            'alef.moraes@hotmail.com.br',
+            'franklin.felipe158@gmail.com',
+            'cesimar.xavier@navi.ifrn.edu.br'];
+
+            foreach ($emails as $email) {
+                Mail::to($email)->send(new ConfirmRegistrationRace());
+            }
+            return redirect('/admin/dashboard');
+
         } catch (Exception $e){
             dd($e);
         }
