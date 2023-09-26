489f46bdc10fb283358049bab051a40a2a35f236
Zadoque Teófilo <zadoque.dev@gmail.com>
2023-09-26 15:44:58 -0300

    .

--- resources/views/PRF/Admin/reports.blade.php
+++ resources/views/PRF/Admin/reports.blade.php
@@ -55,7 +55,7 @@
                   Todos os usuários
                 </p>
               </a>
-{{-- 
+{{--
               <a href=" /admin/all_pending_registrations_get" class="order-1 sm:order-2 flex items-center justify-center sm:justify-start gap-4 w-full sm:w-fit px-4 py-2.5 rounded-lg border-[1.5px] border-brand-prfA1 hover:ring-2 hover:ring-brand-prfA1 hover:ring-opacity-50 bg-brand-prfA1 transition">
                 <img src="/images/svg/download.svg" alt="">
                 <p class="text-white text-sm font-bold font-poppins">
@@ -63,12 +63,12 @@
                 </p>
               </a> --}}
 
-              <a href="/admin/all_servidores_get" class="order-1 sm:order-2 flex items-center justify-center sm:justify-start gap-4 w-full sm:w-fit px-4 py-2.5 rounded-lg border-[1.5px] border-brand-prfA1 hover:ring-2 hover:ring-brand-prfA1 hover:ring-opacity-50 bg-brand-prfA1 transition">
+              {{-- <a href="/admin/all_servidores_get" class="order-1 sm:order-2 flex items-center justify-center sm:justify-start gap-4 w-full sm:w-fit px-4 py-2.5 rounded-lg border-[1.5px] border-brand-prfA1 hover:ring-2 hover:ring-brand-prfA1 hover:ring-opacity-50 bg-brand-prfA1 transition">
                 <img src="/images/svg/download.svg" alt="">
                 <p class="text-white text-sm font-bold font-poppins">
                   Servidores
                 </p>
-              </a>
+              </a> --}}
             </div>
           </div>
 
