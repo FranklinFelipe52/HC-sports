3f72286e0a56b9decbaf3aded7973d02d36fd6c9
Zadoque Teófilo <zadoque.dev@gmail.com>
2023-10-05 18:01:50 -0300

    alterando lógica da função de cálculo de idade para descontos

--- app/Helpers/AgeBetweenDates.php
+++ app/Helpers/AgeBetweenDates.php
@@ -1,35 +1,52 @@
 <?php
 
 namespace App\Helpers;
+
+use DateTime;
+
 class AgeBetweenDates
 {
-    
-    public static function calc_idade( $data_nasc, $data_referencia ){
+
+    public static function calc_idade($dataNascimento)
+    {
+        $dataNascimento = new DateTime($dataNascimento);
+
+        $anoAtual = date("Y");
+
+        $ultimoDiaDoAno = new DateTime("$anoAtual-12-31");
+
+        $diferenca = $dataNascimento->diff($ultimoDiaDoAno);
+
+        return $diferenca->y;
+    }
+
+    public static function calc_idade_old($data_nasc, $data_referencia)
+    {
         $data_nasc = date("d-m-Y", strtotime($data_nasc));
         $data_nasc = explode("-", $data_nasc);
         $data = date("d-m-Y", strtotime($data_referencia));
         $data = explode("-", $data);
-        
+
         $anos = $data[2] - $data_nasc[2];
-        
-        if ( $data_nasc[1] >= $data[1] ){
-        
-        if ( $data_nasc[0] <= $data[0] ){
-        
-        return $anos;
-        
-        }else{
-        
-        return $anos-1;
-        
+
+        if ($data_nasc[1] >= $data[1]) {
+
+            if ($data_nasc[0] <= $data[0]) {
+
+                return $anos;
+
+            } else {
+
+                return $anos - 1;
+
+            }
+
+        } else {
+
+            return $anos;
+
         }
-        
-        }else{
-        
-        return $anos;
-        
-        }
-        
-        }
+
+    }
 }
 ?>
