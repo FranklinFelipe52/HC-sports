0d281ffbc51fe33a646a45d4a08f211199699aec
Zadoque Teófilo <zadoque.dev@gmail.com>
2023-10-05 17:02:04 -0300

    alterando base do cálculo de descontos para idoso

--- app/Helpers/ValorTotal.php
+++ app/Helpers/ValorTotal.php
@@ -8,9 +8,9 @@ use Faker\Core\Number;
 
 class ValorTotal
 {
-    
+
     public static function ValorComDescontos( PrfUser $user, PrfRegistration $registration){
-            
+
                 $priceTshirts = 0;
                 foreach ( $registration->tshirts as $tshirt) {
                     $priceTshirts = $priceTshirts + $tshirt->price;
@@ -18,29 +18,29 @@ class ValorTotal
                 $priceRegistration = $registration->prf_categorys->price;
                 $valor_bruto = $priceRegistration;
                 $descontos = 0;
-                if(AgeBetweenDates::calc_idade($user->data_nasc, now()) >= 60 || $user->prf_deficiency_id){
+                if(AgeBetweenDates::calc_idade($user->data_nasc, now()) >= 61 || $user->prf_deficiency_id){
                     $descontos = 0.5;
                 }
                 if($registration->prf_vauchers){
                     $descontos = $registration->prf_vauchers->desconto > $descontos ? $registration->prf_vauchers->desconto : $descontos;
                 }
-                
+
                 $valor_bruto = $valor_bruto - ($valor_bruto*$descontos);
                 $valor_bruto = $valor_bruto + $priceTshirts;
-            
+
         return $valor_bruto;
     }
 
     public static function DescontosTotais( PrfUser $user, PrfRegistration $registration){
-            
+
         $descontos = 0;
-        if(AgeBetweenDates::calc_idade($user->data_nasc, "28-12-".date("Y")) >= 60 || $user->prf_deficiency_id){
+        if(AgeBetweenDates::calc_idade($user->data_nasc, "28-12-".date("Y")) >= 61 || $user->prf_deficiency_id){
             $descontos = 0.5;
         }
         if($registration->prf_vauchers){
             $descontos = $registration->prf_vauchers->desconto > $descontos ? $registration->prf_vauchers->desconto : $descontos;
         }
-    
+
 return $descontos;
 }
 }
