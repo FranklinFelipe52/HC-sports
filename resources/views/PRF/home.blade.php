8148e5e19d0c667b8c97c9c0da45ac6264764b06
tzadoque <zadoque.dev@gmail.com>
2024-02-05 15:20:57 -0300

    fix: atualizando vitrine

--- resources/views/PRF/home.blade.php
+++ resources/views/PRF/home.blade.php
@@ -44,8 +44,8 @@
     </header>
 
     <main class="container grow pt-6 pb-32">
-      <img src="/images/PRF/banner-mobile.png" class="md:hidden w-full" alt="">
-      <img src="/images/PRF/banner-desktop.png" class="hidden md:block w-full" alt="">
+      {{-- <img src="/images/PRF/banner-mobile.png" class="md:hidden w-full" alt=""> --}}
+      <img src="/images/CAERN/vitrine.png" class="block w-full" alt="">
       <div class="grid grid-cols-1 lg:grid-cols-2 pt-8 gap-6">
         <div class="order-2 lg:order-1">
           <div class="mb-12">
