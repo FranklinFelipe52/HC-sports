c0dca01b0325f9d1a4887ecb41ad0d4202f56085
Franklin Felipe <f.franklin@escolar.ifrn.edu.br>
2024-10-26 13:44:18 -0300

    coluna de encerramento

---
+++ database/migrations/2024_10_26_134141_edit_prf_categorys_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('prf_categorys', function (Blueprint $table) {
+            $table->boolean('registrations_closed')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
